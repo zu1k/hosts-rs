361116db789162e147cdb5fe9c691b154a936bd3
zu1k <i@lgf.im>
2021-07-14 21:57:02 +0800

    write hosts to file

--- resolve-github/src/main.rs
+++ resolve-github/src/main.rs
@@ -1,7 +1,15 @@
 mod dns;
 use dns::*;
+use std::{fs, io::Write};
 
 fn main() {
     let hosts = resolve_github_assets();
-    println!("{:#?}", hosts);
+    if let Ok(mut file) = fs::File::create("hosts") {
+        match file.write_all(hosts.to_string().as_bytes()) {
+            Ok(_) => println!("Success"),
+            Err(err) => eprintln!("Error: {}", err),
+        }
+    } else {
+        println!("{:#?}", hosts);
+    }
 }
