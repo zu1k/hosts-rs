4424fbf38956278347aca7257a71e6f7cf7aeaa7
zu1k <i@lgf.im>
2021-07-14 14:22:45 +0800

    add hosts src

--- src/req.rs
+++ src/req.rs
@@ -3,7 +3,8 @@ use reqwest::blocking::Client;
 
 const USER_AGENT: &str = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36";
 
-pub const GITHUB_520: [&str;2] = [
+pub const GITHUB_520: [&str;3] = [
+    "https://api.hellogithub.com/GitHub520/hosts",
     "https://raw.hellogithub.com/hosts", 
     "https://raw.githubusercontent.com/521xueweihan/GitHub520/main/hosts"
 ];
@@ -17,7 +18,7 @@ pub fn fetch() -> Option<String> {
     for url in GITHUB_520 {
         if let Ok(resp) = client.get(url).send() {
             if let Ok(body) = resp.text() {
-                println!("{}", url);
+                println!("valid url: {}", url);
                 return Some(body)
             }
         }
