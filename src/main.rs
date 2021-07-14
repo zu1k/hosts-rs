4a7153119c89acdcde57540c882c08786778f1d4
zu1k <i@lgf.im>
2021-07-14 11:34:48 +0800

    remove useless code

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,11 @@
 mod req;
 mod parse;
 mod file;
-use req::fetch;
+use req::*;
 use parse::Hosts;
 
 fn main() {
-    let hosts = fetch("https://raw.githubusercontent.com/521xueweihan/GitHub520/main/hosts").unwrap();
+    let hosts = fetch(GITHUB_520).unwrap();
     let hosts = Hosts::from(hosts.as_str());
-    println!("{:#?}", hosts);
     println!("{}", hosts);
-    println!("len: {}", hosts.data.len());
 }
--- src/req.rs
+++ src/req.rs
@@ -3,6 +3,8 @@ use reqwest::blocking::Client;
 
 const USER_AGENT: &str = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36";
 
+pub const GITHUB_520: &str = "https://raw.githubusercontent.com/521xueweihan/GitHub520/main/hosts";
+
 pub fn fetch(url: &str) -> Result<String, Box<dyn std::error::Error>> {
     let client = Client::builder()
         .timeout(Duration::from_secs(5))
